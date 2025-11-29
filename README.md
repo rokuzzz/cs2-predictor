ace01533ae78aa0f39de36c0499eb5404b0608d3
Roman Kuzero <rombeldan@gmail.com>
2025-11-29 06:01:27 +0200

    chore: initial monorepo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# CS2 Predictor Monorepo
+
+A full-stack application for predicting CS2 tournament outcomes.
---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    // Note: there should be no other properties in this object
+    ignores: [
+      'coverage',
+      '**/public',
+      '**/dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
